af73791c790e37f0edd2eb7fd7496b97f2776485
Benjamin Crosnier <benjamin.crosnier@signature-code.com>
2019-09-30 15:04:39 +0200

    Add ModifyAsync extensions

--- CK.Observable.Domain/ObservableDomainExtensions.cs
+++ CK.Observable.Domain/ObservableDomainExtensions.cs
@@ -23,5 +23,47 @@ namespace CK.Observable
 
             return tr;
         }
+
+        /// <summary>
+        /// Modifies this ObservableDomain, then executes any pending resulting actions.
+        /// </summary>
+        /// <typeparam name="TRoot1">The type of the ObservableRootObject #1.</typeparam>
+        /// <typeparam name="TRoot2">The type of the ObservableRootObject #2.</typeparam>
+        /// <param name="this">The ObservableDomain.</param>
+        /// <param name="monitor">The ActivityMonitor.</param>
+        /// <param name="actions">The actions to execute inside the ObservableDomain's modification context.</param>
+        /// <returns>The transaction result from <see cref="ObservableDomain.Modify"/>.</returns>
+        public static async Task<TransactionResult> ModifyAsync<TRoot1, TRoot2>( this ObservableDomain<TRoot1, TRoot2> @this, IActivityMonitor monitor, Action actions )
+            where TRoot1 : ObservableRootObject
+            where TRoot2 : ObservableRootObject
+        {
+            var tr = @this.Modify( monitor, actions );
+
+            await tr.ExecutePostActionsAsync( monitor );
+
+            return tr;
+        }
+
+        /// <summary>
+        /// Modifies this ObservableDomain, then executes any pending resulting actions.
+        /// </summary>
+        /// <typeparam name="TRoot1">The type of the ObservableRootObject #1.</typeparam>
+        /// <typeparam name="TRoot2">The type of the ObservableRootObject #2.</typeparam>
+        /// <typeparam name="TRoot3">The type of the ObservableRootObject #3.</typeparam>
+        /// <param name="this">The ObservableDomain.</param>
+        /// <param name="monitor">The ActivityMonitor.</param>
+        /// <param name="actions">The actions to execute inside the ObservableDomain's modification context.</param>
+        /// <returns>The transaction result from <see cref="ObservableDomain.Modify"/>.</returns>
+        public static async Task<TransactionResult> ModifyAsync<TRoot1, TRoot2, TRoot3>( this ObservableDomain<TRoot1, TRoot2, TRoot3> @this, IActivityMonitor monitor, Action actions )
+            where TRoot1 : ObservableRootObject
+            where TRoot2 : ObservableRootObject
+            where TRoot3 : ObservableRootObject
+        {
+            var tr = @this.Modify( monitor, actions );
+
+            await tr.ExecutePostActionsAsync( monitor );
+
+            return tr;
+        }
     }
 }
