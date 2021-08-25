5f0d680d02f34793058ffe98e14c7aed71b7c83c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-08-25 12:54:11 +0200

    Added logs to parrallel_operations_respect_the_Domain_PostActions_ordering_guaranty.

--- Tests/CK.Observable.Domain.Tests/PostActionsTests.cs
+++ Tests/CK.Observable.Domain.Tests/PostActionsTests.cs
@@ -227,7 +227,9 @@ namespace CK.Observable.Domain.Tests
         {
             ResetContext();
 
-            using var d = new ObservableDomain<SimpleRoot>( TestHelper.Monitor, "local_are_executed_before_domain_ones", startTimer: true );
+            using var d = new ObservableDomain<SimpleRoot>( TestHelper.Monitor,
+                                                            "local_are_executed_before_domain_ones_when_parallelDomainPostActions_is_false",
+                                                            startTimer: true );
 
             await d.ModifyThrowAsync( TestHelper.Monitor, () =>
             {
@@ -263,7 +265,7 @@ namespace CK.Observable.Domain.Tests
         {
             ResetContext();
 
-            using var d = new ObservableDomain<SimpleRoot>( TestHelper.Monitor, "parrallel_operations_respect_the_Domain_PostActions_ordering_guaranty", startTimer: true );
+            using var d = new ObservableDomain<SimpleRoot>( TestHelper.Monitor, $"parrallel_operations_respect_the_Domain_PostActions_ordering_guaranty-{nb}-{useAsync}", startTimer: true );
 
             Barrier b = new Barrier( nb );
             var tasks = Enumerable.Range( 0, nb ).Select( i => Task.Run( () => Run( i, i == 0 ? TestHelper.Monitor : new ActivityMonitor(), d, b ) ) ).ToArray();
@@ -274,10 +276,13 @@ namespace CK.Observable.Domain.Tests
 
             async Task Run( int num, IActivityMonitor monitor, ObservableDomain<SimpleRoot> d, Barrier b )
             {
+                monitor.Info( $"Run {num}: Waiting for Barrier..." );
                 b.SignalAndWait();
+                monitor.Info( $"Running {num}!" );
                 var tr = await d.ModifyThrowAsync( monitor, () =>
                 {
                     if( (num % 2) == 0 ) d.Root.SendWait( 500, WaitTarget.PostActions );
+                    if( (num % 5) == 0 ) d.Root.SendWait( 100, WaitTarget.DomainPostActions );
                     d.Root.SendNumber( HandlerTarget.Local, useAsync );
                     d.Root.SendNumber( HandlerTarget.Domain, useAsync );
                     d.Root.SendNumber( HandlerTarget.Local, useAsync );
@@ -285,7 +290,9 @@ namespace CK.Observable.Domain.Tests
                     d.Root.SendNumber( HandlerTarget.Local, useAsync );
                     d.Root.SendNumber( HandlerTarget.Domain, useAsync );
                 } );
+                monitor.Info( $"Run {num}: Waiting for DomainPostActionsError..." );
                 await tr.DomainPostActionsError;
+                monitor.Info( $"Awaited DomainPostActionsError! (Run {num})" );
             }
         }
 
