3592e9b964b6d9422d0219b9da6fe5d7c48bf70d
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-06-13 21:25:33 +0200

    Fixed mqtt. @types/node is required.

--- CK.Observable.MQTTWatcher/TSPackage.cs
+++ CK.Observable.MQTTWatcher/TSPackage.cs
@@ -1,9 +1,11 @@
 using CK.StObj.TypeScript;
+using System.Xml.Linq;
 
 namespace CK.Observable.MQTTWatcher
 {
     [TypeScriptPackage]
-    [ImportTypeScriptLibrary("mqtt", "5.7.0", DependencyKind.Dependency)]
+    [ImportTypeScriptLibrary( "mqtt", "5.7.0", DependencyKind.Dependency )]
+    [ImportTypeScriptLibrary( "@types/node", "^20.14.2", DependencyKind.DevDependency )]
     [TypeScriptFile( "MQTTObservableLeagueDomainService.ts" )]
     public class TSPackage : TypeScriptPackage
     {
