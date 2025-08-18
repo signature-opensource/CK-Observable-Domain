3e92ac243d77488500ce60a82aaca963626cf684
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-08-18 08:09:25 +0200

    Removed ForceUse.

--- CK.Observable.MQTTWatcher/TSPackage.cs
+++ CK.Observable.MQTTWatcher/TSPackage.cs
@@ -4,8 +4,8 @@ using System.Xml.Linq;
 namespace CK.Observable.MQTTWatcher;
 
 [TypeScriptPackage]
-[TypeScriptImportLibrary("mqtt", "5.10.1", DependencyKind.PeerDependency, ForceUse = true)]
-[TypeScriptImportLibrary( "@types/node", "^20.14.2", DependencyKind.DevDependency, ForceUse = true )]
+[TypeScriptImportLibrary("mqtt", "5.10.1", DependencyKind.PeerDependency )]
+[TypeScriptImportLibrary( "@types/node", "^20.14.2", DependencyKind.DevDependency )]
 [TypeScriptFile( "MQTTObservableLeagueDomainService.ts", "MQTTObservableLeagueDomainService" )]
 public class TSPackage : TypeScriptPackage
 {
--- CK.Observable.SignalRWatcher/TSPackage.cs
+++ CK.Observable.SignalRWatcher/TSPackage.cs
@@ -3,7 +3,7 @@ using CK.TypeScript;
 namespace CK.Observable.SignalRWatcher;
 
 [TypeScriptPackage]
-[TypeScriptImportLibrary( "@microsoft/signalr", ">=6.0.23", DependencyKind.PeerDependency, ForceUse = true )]
+[TypeScriptImportLibrary( "@microsoft/signalr", ">=6.0.23", DependencyKind.PeerDependency )]
 [TypeScriptFile( "SignalRObservableLeagueDomainService.ts", "SignalRObservableLeagueDomainService" )]
 public class TSPackage : TypeScriptPackage
 {
